d3d33d6d43dfe453df7462e9a5ffc9ec114f40a6
Akanbi Rahmon <23046879+Rahmon231@users.noreply.github.com>
2022-09-09 00:48:38 +0100

    model class

---
+++ app/src/main/java/com/lemzeeyyy/pagginglibrarydemo/model/Movie.java
@@ -0,0 +1,57 @@
+package com.lemzeeyyy.pagginglibrarydemo.model;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.Objects;
+
+public class Movie {
+    @SerializedName("id")
+    @Expose
+    private int id;
+
+    @SerializedName("poster_path")
+    @Expose
+    private String posterPath;
+
+    @SerializedName("vote_average")
+    @Expose
+    private double voteAverage;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getPosterPath() {
+        return posterPath;
+    }
+
+    public void setPosterPath(String posterPath) {
+        this.posterPath = posterPath;
+    }
+
+    public double getVoteAverage() {
+        return voteAverage;
+    }
+
+    public void setVoteAverage(double voteAverage) {
+        this.voteAverage = voteAverage;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Movie movie = (Movie) o;
+        return id == movie.id && Double.compare(movie.voteAverage, voteAverage) == 0 && posterPath.equals(movie.posterPath);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, posterPath, voteAverage);
+    }
+}
